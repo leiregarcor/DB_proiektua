caa4eba0b15d73c7bf60ac5e5c5d6b7dcf66b134
Duxon900 <quintanojon@gmail.com>
2021-04-12 22:10:15 +0200

    login pantaila

---
+++ app/src/main/java/DB_proiektua/UIKudeatzaile/LogInKud.java
@@ -0,0 +1,23 @@
+package DB_proiektua.UIKudeatzaile;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LogInKud {
+
+    @FXML
+    private TextField txtIzena;
+
+    @FXML
+    private PasswordField txtPass;
+
+    @FXML
+    void onClick(ActionEvent event) {
+        //TODO: klik egitean zer egingo du botoiak:)
+
+
+    }
+
+}
