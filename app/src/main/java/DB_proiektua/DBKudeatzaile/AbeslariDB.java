83f37ecc16489d6c7df27316e60e0a8ea28c8bac
Asi <asierrosa@gmail.com>
2021-04-22 12:33:30 +0200

    A

---
+++ app/src/main/java/DB_proiektua/DBKudeatzaile/AbeslariDB.java
@@ -0,0 +1,66 @@
+package DB_proiektua.DBKudeatzaile;
+
+import DBKudeatzailea.DBKudeatzaile;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
+public class AbeslariDB {
+
+    private static AbeslariDB instantzia = new AbeslariDB();
+
+    public static AbeslariDB getInstance(){
+        return instantzia;
+    }
+
+    public List<String> ikusiAbestiak(int id){
+        DBKudeatzaile dbKudeatzaile = DBKudeatzaile.getInstantzia();
+        String lortuCMSAtributuak = "select abestiak from ParteHartzailea where id="+id;
+        ResultSet rs = dbKudeatzaile.execSQL(lortuCMSAtributuak);
+        List<String> emaitza = new ArrayList<>();
+
+        try{
+            while(rs.next()) {
+                String izena = rs.getString("izena");
+            }
+        }catch(SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return emaitza;
+    }
+
+    public String ikusiNonOstatu(int id){
+        DBKudeatzaile dbKudeatzaile = DBKudeatzaile.getInstantzia();
+        String lortuCMSAtributuak = "select fk_ParteHartzaile_Herria from ParteHartzaile where id="+id;
+        ResultSet rs = dbKudeatzaile.execSQL(lortuCMSAtributuak);
+        String izena = "";
+        try{
+            while(rs.next()) {
+                izena = rs.getString("izena");
+            }
+        }catch(SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return izena;
+    }
+
+    public List<String> ikusiRanking(){
+        DBKudeatzaile dbKudeatzaile = DBKudeatzaile.getInstantzia();
+        String lortuCMSAtributuak = "select izena" +
+                " from ParteHartzaile" +
+                " order by puntuazioa asc" +
+                "LIMIT 2, 1) x";
+        ResultSet rs = dbKudeatzaile.execSQL(lortuCMSAtributuak);
+        List<String> emaitza = new ArrayList<>();
+
+        try{
+            while(rs.next()) {
+                String izena = rs.getString("izena");
+            }
+        }catch(SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return emaitza;
+    }
+}
--- app/src/main/java/DB_proiektua/UIKudeatzaile/AbeslariKud.java
+++ app/src/main/java/DB_proiektua/UIKudeatzaile/AbeslariKud.java
@@ -1,12 +1,46 @@
 package DB_proiektua.UIKudeatzaile;
 
 import DB_proiektua.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import org.checkerframework.checker.initialization.qual.Initialized;
+import DB_proiektua.*;
+import DB_proiektua.DBKudeatzaile.AbeslariDB;
+
+import java.awt.*;
 
 public class AbeslariKud {
+
+    @FXML
+    private Label lbIzena;
+
+    @FXML
+    private Label lblOstatu;
+
+    @FXML
+    private Button btnAbestiak;
+
+    @FXML
+    private Button btnRanking;
+
 
     private Main main;
 
     public void setMain(Main mainApp){
         this.main=mainApp;
     }
+
+    @FXML
+    void OnClick(ActionEvent event) {
+        Button btn = (Button)event.getSource();
+
+        if(btn.equals(btnAbestiak)){
+            AbeslariDB.getInstance().ikusiAbestiak();
+        }
+        if(btn.equals(btnRanking)){
+            AbeslariDB.getInstance().ikusiRanking();
+        }
+    }
+
+
 }
