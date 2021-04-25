3dff9d66a6bab0abd5ae126ab4f1698a1f53288e
Duxon900 <quintanojon@gmail.com>
2021-04-25 22:53:58 +0200

    kode garbiketa + warning kendu

--- app/src/main/java/DB_proiektua/DBKudeatzailea/AbeslariQuery/AbeslariDB.java
+++ app/src/main/java/DB_proiektua/DBKudeatzailea/AbeslariQuery/AbeslariDB.java
@@ -62,41 +62,4 @@ public class AbeslariDB {
     }
 
 
-
-    /*
-    public String ikusiNonOstatu(int id){
-        DBKudeatzaile dbKudeatzaile = DBKudeatzaile.getInstantzia();
-        String lortuCMSAtributuak = "select fk_ParteHartzaile_Herria from ParteHartzaile where id="+id;
-        ResultSet rs = dbKudeatzaile.execSQL(lortuCMSAtributuak);
-        String izena = "";
-        try{
-            while(rs.next()) {
-                izena = rs.getString("izena");
-            }
-        }catch(SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return izena;
-    }
-
-    public List<String> ikusiRanking(){
-        DBKudeatzaile dbKudeatzaile = DBKudeatzaile.getInstantzia();
-        String lortuCMSAtributuak = "select izena" +
-                " from ParteHartzaile" +
-                " order by puntuazioa asc";
-        ResultSet rs = dbKudeatzaile.execSQL(lortuCMSAtributuak);
-        List<String> emaitza = new ArrayList<>();
-
-        try{
-            while(rs.next()) {
-                String izena = rs.getString("izena");
-            }
-        }catch(SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return emaitza;
-    }
-     */
-
-
 }
