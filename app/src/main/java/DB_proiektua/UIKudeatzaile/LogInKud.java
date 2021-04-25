8e1295c6debaaf0dc42b5649a7acbf9b23d576e8
arrosa <asierrosa@gmail.com>
2021-04-25 22:52:09 +0200

    limpieza de codigo

--- app/src/main/java/DB_proiektua/UIKudeatzaile/LogInKud.java
+++ app/src/main/java/DB_proiektua/UIKudeatzaile/LogInKud.java
@@ -81,7 +81,7 @@ public class LogInKud {
 
     }
 
-    //Javan String bat zifratzeko
+    //Javan String bat zifratzekoa
     private String zifratuGakoa(String text) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         MessageDigest digest = MessageDigest.getInstance("MD5");
         byte[] hashedBytes = digest.digest(text.getBytes("UTF-8"));
