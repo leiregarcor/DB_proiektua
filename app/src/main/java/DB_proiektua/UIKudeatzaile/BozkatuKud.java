8ec34a5c5bbff3765b36c8cc4bf302ea31c742e7
leire <leiregarcor@gmail.com>
2021-04-26 18:01:14 +0200

    badoa

--- app/src/main/java/DB_proiektua/UIKudeatzaile/BozkatuKud.java
+++ app/src/main/java/DB_proiektua/UIKudeatzaile/BozkatuKud.java
@@ -1,11 +1,13 @@
 package DB_proiektua.UIKudeatzaile;
 
+import DBKudeatzailea.DBKudeatzaile;
 import DB_proiektua.Main;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 
 import java.net.URL;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 
 public class BozkatuKud implements Initializable {
@@ -21,6 +23,10 @@ public class BozkatuKud implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //select p.Izena, a.izena  from ParteHartzaile p, Abestia a, Erregistratu where a.ParteHartzaileID=p.id and YEAR(ErregistroData)=YEAR(curdate())
+        String query="select p.Izena as PHIzena, a.izena as AbestiIzena  from ParteHartzaile p, Abestia a, Erregistratu where a.ParteHartzaileID=p.id and YEAR(ErregistroData)=YEAR(curdate())";
+
+        ResultSet rs=DBKudeatzaile.getInstantzia().execSQL(query);
 
     }
 }
